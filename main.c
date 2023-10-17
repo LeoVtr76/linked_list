fcfbd6ff63daa8fdba5c55bbc7af274fffbc8f13
LeoVtr76 <leo.vauthier76@gmail.com>
2023-10-17 10:24:11 +0200

    Update UI

--- main.c
+++ main.c
@@ -10,6 +10,11 @@ void printList(data *_head){
         printf("%d ", _head->value);
         _head = _head->next;
     }
+    printf("\n");
+}
+void insert (){
+    data * new_value = NULL;
+    new_value =(data*)malloc (sizeof(data));
 }
 int main() {
     data *head = NULL;
@@ -25,7 +30,7 @@ int main() {
     d = (data *)malloc(sizeof(data));  
     head = a; 
 
-    a->value = 12; 
+    a->value = 12;
     a->next = b;
 
     b->value = 7; 
@@ -39,7 +44,7 @@ int main() {
     
     data *e = NULL;
     e = (data*) malloc(sizeof(data));
-    printf("Entrez une valeur");
+    printf("Entrez une valeur : ");
     scanf("%d", &e->value);
     b->next = e;
     e->next = c;
